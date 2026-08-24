89bbb5ef7029f805d599580e05900eb79195b01f
Dreamhacker-Fear <lucaebraeuer@gmail.com>
2026-08-24 15:34:40 +0200

    Refactor auto formatting and add logging
    Removed formatting logic and integrated logging for loading and unloading.

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -1,52 +1,11 @@
-import { metro } from "@vendetta/metro/common";
-import { storage } from "@vendetta/plugin";
-import { before } from "@vendetta/patcher";
-import Settings from "./Settings";
-
-let unpatch: (() => void) | undefined;
-
-function formatMessage(content: string) {
-    let result = content;
-
-    if (storage.autoBold) result = `**${result}**`;
-    if (storage.autoItalic) result = `*${result}*`;
-    if (storage.autoSpoiler) result = `||${result}||`;
-    if (storage.autoCode) result = `\`${result}\``;
-    if (storage.autoUnderline) result = `__${result}__`;
-    if (storage.autoStrikethrough) result = `~~${result}~~`;
-    if (storage.autoQuote) {
-        result = result
-            .split("\n")
-            .map((line: string) => `> ${line}`)
-            .join("\n");
-    }
-
-    return result;
-}
+import { logger } from "@vendetta";
 
 export default {
     onLoad() {
-        const MessageActions = metro.findByProps("sendMessage");
-
-        if (!MessageActions) return;
-
-        unpatch = before(
-            "sendMessage",
-            MessageActions,
-            (args: any[]) => {
-                const message = args?.[1];
-
-                if (!message || typeof message.content !== "string") return;
-
-                message.content = formatMessage(message.content);
-            }
-        );
+        logger.log("Auto Formatter loaded");
     },
 
     onUnload() {
-        unpatch?.();
-        unpatch = undefined;
+        logger.log("Auto Formatter unloaded");
     },
-
-    settings: Settings,
 };
