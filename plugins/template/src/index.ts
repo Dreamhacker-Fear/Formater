ad2774c4e4d237ec34fff5d4107b2aa90ae07395
Dreamhacker-Fear <lucaebraeuer@gmail.com>
2026-08-24 14:34:03 +0200

    Implement message formatting in onLoad
    Added message formatting options based on user settings.

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -1,12 +1,61 @@
-import { logger } from "@vendetta";
-import Settings from "./Settings";
+import { before } from "@vendetta/patcher";
+import { findByProps } from "@vendetta/metro";
+import Settings, { settings } from "./Settings";
+
+const MessageActions = findByProps("sendMessage", "editMessage");
+
+let unpatch: (() => void) | undefined;
+
+function formatMessage(content: string) {
+    if (!content || typeof content !== "string") return content;
+
+    let result = content;
+
+    if (settings.bold)
+        result = `**${result}**`;
+
+    if (settings.italic)
+        result = `*${result}*`;
+
+    if (settings.underline)
+        result = `__${result}__`;
+
+    if (settings.strikethrough)
+        result = `~~${result}~~`;
+
+    if (settings.spoiler)
+        result = `||${result}||`;
+
+    if (settings.code)
+        result = `\`${result}\``;
+
+    if (settings.quote)
+        result = result
+            .split("\n")
+            .map(line => `> ${line}`)
+            .join("\n");
+
+    return result;
+}
 
 export default {
-    onLoad: () => {
-        logger.log("Hello world!");
+    onLoad() {
+        if (!MessageActions?.sendMessage) return;
+
+        unpatch = before("sendMessage", MessageActions, args => {
+            const message = args?.[1];
+
+            if (!message || typeof message.content !== "string")
+                return;
+
+            message.content = formatMessage(message.content);
+        });
     },
-    onUnload: () => {
-        logger.log("Goodbye, world.");
+
+    onUnload() {
+        unpatch?.();
+        unpatch = undefined;
     },
-    settings: Settings,
-}
+
+    settings: Settings
+};
